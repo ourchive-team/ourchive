93df5468bb1d74f8b42fb20612d54c7b44ef26be
Sanghyeok (Dave Kim) <sanghyeokkim0522@gmail.com>
2023-04-09 01:44:59 +0900

    fix: delete unused code

--- src/Pages/Report/index.tsx
+++ src/Pages/Report/index.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { baseColor, LargeButton, PaddingBox, StyledInput } from '../../styles';
-import { getImageInfo, reportImage } from '../../func';
 import { onchain } from '../../func';
 
 import TopNavigator from '../../Components/TopNavigator';
