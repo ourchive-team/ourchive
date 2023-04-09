71ab81544d84b1d3cfe5de813e59517b286c6846
Sanghyeok (Dave Kim) <sanghyeokkim0522@gmail.com>
2023-04-10 01:27:32 +0900

    Update sendMail.ts

--- src/func/sendMail.ts
+++ src/func/sendMail.ts
@@ -1,9 +1,9 @@
 import emailjs from 'emailjs-com';
 
 export function sendMail({ toEmail, imageTitle, creatorNickname, phrase, imageUrl, proveUrl }: any) {
-  const SERVICE_ID = 'service_0ty64aq'; // emailjs.com에서 생성한 서비스 ID
-  const TEMPLATE_ID = 'template_2wegq7b'; // emailjs.com에서 생성한 템플릿 ID
-  const USER_ID = 'kcLTrHuxtIzHjpYWE';
+  const SERVICE_ID = 'service_sxcooba'; // emailjs.com에서 생성한 서비스 ID
+  const TEMPLATE_ID = 'template_wachdrr'; // emailjs.com에서 생성한 템플릿 ID
+  const PUBLIC_KEY = 'Zxxzo3ndUSqKmnT3J';
 
   const emailParams = {
     toEmail,
@@ -14,7 +14,7 @@ export function sendMail({ toEmail, imageTitle, creatorNickname, phrase, imageUr
     proveUrl,
   };
 
-  emailjs.send(SERVICE_ID, TEMPLATE_ID, emailParams, USER_ID).then(
+  emailjs.send(SERVICE_ID, TEMPLATE_ID, emailParams, PUBLIC_KEY).then(
     response => {
       console.log('SUCCESS!', response.status, response.text);
     },
